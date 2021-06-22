4c365165010d77fb15d1406cc177b0151d684f73
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-06-22 10:25:09 -0400

    Add CLI class and executable

--- lib/ruby_to_uml.rb
+++ lib/ruby_to_uml.rb
@@ -1,4 +1,5 @@
 require_relative 'ruby_to_uml/uml_diagram_renderer/uml_diagram_renderer.rb'
 require_relative 'ruby_to_uml/uml_info_generator/uml_info_generator.rb'
+require_relative 'ruby_to_uml/cli.rb'
 require_relative 'ruby_to_uml/nomnoml_dsl_generator.rb'
 require_relative 'ruby_to_uml/path_transformer.rb'
---
+++ lib/ruby_to_uml/cli.rb
@@ -0,0 +1,9 @@
+module CLI
+  def self.start(arguments)
+    abort('Usage: ruby_to_uml [source directory or file]') if arguments.empty?
+    file_paths = PathTransformer.transform_files_and_or_directories_paths_to_file_paths(arguments)
+    uml_info = UMLInfoGenerator.process_files(file_paths)
+    dsl = NomnomlDSLGenerator.generate_dsl(uml_info)
+    UMLDiagramRenderer.create_diagram(dsl.to_s)
+  end
+end
